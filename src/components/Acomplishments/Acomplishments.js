b1be095fdbdab46ac4464a42826b7c1a39c3c5a5
Hao Li <80303979+HaoLI-jpg@users.noreply.github.com>
2022-02-16 23:03:10 +1300

    add photo gallary

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -2,13 +2,38 @@ import React from 'react';
 
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import Gallery from "react-photo-gallery";
 
 const data = [
   { number: 1, text: 'Years into web development'},
   { number: 3, text: 'Years into programming'},
   { number: 3, text: 'Years into photography'}
 ];
+const photos = [
+  {
+    src: '/images/DSCF2500-edited.JPG',
+    width: 5,
+    height: 2.5
+  },
+  {
+    src: '/images/DSCF2219-edited.JPG',
+    width: 4,
+    height: 6
+  },
+  {
+    src: '/images/DSCF2241-edited.JPG',
+    width: 4,
+    height: 6
+  }, 
+  {
+    src: '/images/DSCF2578-edited.JPG',
+    width: 1,
+    height: 1
+  },
+  
+   
 
+];
 const Acomplishments = () => (
   <Section>
     <SectionTitle>Personal Life</SectionTitle>
@@ -21,10 +46,7 @@ const Acomplishments = () => (
       ))}
     </Boxes>
     <SectionText>I like spending time on photography and coding in my spare time. I often spend time in nature and cities to capture all the wonderful moments and views with my camera.</SectionText>
-    <div >
-    <img src="/images/DSCF2219-edited.JPG" width="50%" height="50%"/>
-    <img src="/images/DSCF2241-edited.JPG" width="50%" height="50%"/>
-    </div>
+    <Gallery photos={photos} direction={"column"}/>
   </Section>
 );
 
